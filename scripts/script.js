67bf50a654a25b793739313bfd540059a23c3ce9
Maria Luiza M <maria.lmm.975@gmail.com>
2022-02-10 10:26:56 -0300

    Adiciona funções de entrar e permanecer na sala

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,29 @@
-const usuario = prompt('Qual seu nome?');
+let usuario = prompt('Qual seu nome?');
 const chat = document.querySelector('main');
 let ultimaMensagem = null;
+let intervaloUm = null;
+let intervaloDois = null;
 
+function entrarNaSala(){
+    usuario = {
+        name: usuario
+    };
+    const promessa = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants', usuario);
+    promessa.then(carregarMensagens);
+    promessa.then(permanecerNaSala);
+    promessa.catch(checarUsuario);
+}
+
+function enviarUsuario(){
+    const promessa = axios.post('https://mock-api.driven.com.br/api/v4/uol/status', usuario);
+    promessa.catch(sairDaSala);
+}
+
+function checarUsuario(erro){
+    statusCode = erro.response.status;
+    usuario = prompt('Usuário já existe, insira outro.');
+    entrarNaSala();
+}
 
 function carregarMensagens(){
     const promessa = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages');
@@ -46,14 +68,25 @@ function configurarMensagemNormal(mensagem){
 }
 
 function configurarMensagemPrivada(mensagem){
-    // if (mensagem.to === usuario){
+    if (mensagem.to === usuario){
         chat.innerHTML += `
         <div class="mensagem reservada">
             <p class="mensagem"><span class="time">(${mensagem.time}) </span><strong class="nome from">${mensagem.from}</strong> reservadamente para <strong class="nome to">${mensagem.to}</strong>: ${mensagem.type}</p>
         </div>
         `
-    // }
+    }
 }
 
-carregarMensagens();
-setInterval(carregarMensagens, 3000);
+function permanecerNaSala(){
+    intervaloUm = setInterval(enviarUsuario, 5000);
+    intervaloDois = setInterval(carregarMensagens, 3000);
+}
+
+function sairDaSala(){
+    alert('Você foi desconectado, entre novamente.');
+    clearInterval(intervaloUm);
+    clearInterval(intervaloDois);
+    entrarNaSala();
+}
+
+entrarNaSala();
